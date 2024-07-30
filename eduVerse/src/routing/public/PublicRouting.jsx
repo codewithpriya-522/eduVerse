0f7a0fe8caadf12f51036e17a4cac769d7f8a884
codewithpriya-522 <106175873+codewithpriya-522@users.noreply.github.com>
2024-07-30 23:38:12 +0530

    Updated Login page with new UI and added registration route

--- eduVerse/src/routing/public/PublicRouting.jsx
+++ eduVerse/src/routing/public/PublicRouting.jsx
@@ -5,14 +5,16 @@ import {
 
 import Login from "../../pages/public/login/Login";
 import LandingPage from "../../pages/public/landingPage/LandingPage";
+import Registration from "../../pages/public/registration/Registration";
 
 export const PublicRouting = () => {
     return (
         <div className="h-screen w-full">
-                <Routes>
-                        <Route path="/" element={<LandingPage />} />
-                        <Route path="/login" element={<Login />} />
-                </Routes>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/registration" element={<Registration />} />
+            </Routes>
         </div>
     );
 };
