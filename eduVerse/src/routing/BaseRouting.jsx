65a91bfad8ee1d171dafa02ca53ffacc103ec94d
codewithpriya-522 <106175873+codewithpriya-522@users.noreply.github.com>
2024-07-30 22:15:21 +0530

    up

--- eduVerse/src/routing/BaseRouting.jsx
+++ eduVerse/src/routing/BaseRouting.jsx
@@ -1,26 +1,23 @@
 import {
-    BrowserRouter,
-    Route,
-    Routes,
-  } from "react-router-dom";
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import LandingPage from "../pages/public/landingPage/LandingPage";
-  
-  export const BaseRouting = () => {
-    
 
-  
-    return (
-      <div className="h-screen w-full">
-        <BrowserRouter>
-          <Routes>
-            <Route>
-             
-                <Route path="/*" element={<LandingPage />} />
-                </Route>
-             
-          </Routes>
-        </BrowserRouter>
-      </div>
-    );
-  };
-  
+export const BaseRouting = () => {
+
+
+
+  return (
+    <div className="h-screen w-full">
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route path="/*" element={<LandingPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
